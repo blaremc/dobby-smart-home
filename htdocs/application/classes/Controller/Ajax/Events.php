860f6cea893badde9e4ab8964b67551e79e6f0e0
Oleg Mikhaylenko <blaremc@gmail.com>
2015-07-20 01:13:26 +0300

    Добавил обработку нескольких событий

--- htdocs/application/classes/Controller/Ajax/Events.php
+++ htdocs/application/classes/Controller/Ajax/Events.php
@@ -7,9 +7,16 @@ class Controller_Ajax_Events extends Controller_Ajax
     public function action_index()
     {
         $eventBus = new EventBus();
-        $device = Device::factory(Device::getDeviceByAddress('http://' . $_SERVER['REMOTE_ADDR'] . '/' . $_GET['device']));
-        $device->setLastValue($_GET['value']);
-        Dobby::$log->add('http://' . $_SERVER['REMOTE_ADDR'] . '/' . $_GET['device'] . '  (' . $_GET['value'] . ') Device:' . $device->name);
-        $eventBus->trigger(EventBus::DEVICE_CHANGE, $device);
+        if (!is_array($_GET['device'])){
+            $devices = array($_GET['device']);
+        }else{
+            $devices = $_GET['device'];
+        }
+        foreach ($devices as $key => $dev) {
+            $device = Device::factory(Device::getDeviceByAddress('http://' . $_SERVER['REMOTE_ADDR'] . '/' . $dev));
+            $device->setLastValue($_GET['value'][$key]);
+            Dobby::$log->add('http://' . $_SERVER['REMOTE_ADDR'] . '/' . $dev . '  (' . $_GET['value'][$key] . ') Device:' . $device->name);
+            $eventBus->trigger(EventBus::DEVICE_CHANGE, $device);
+        }
     }
 }
